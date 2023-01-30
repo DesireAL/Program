7f8863651080f1516e0a29249a9975cca6bbbd2a
DesireAL <894624020@qq.com>
2023-01-30 21:05:53 +0800

    23.1.30 learn

---
+++ JAVA/Chapter8/src/com/mingrisoft/test/MyTest.java
@@ -0,0 +1,7 @@
+package com.mingrisoft.test;
+import static java.lang.System.out;
+public class MyTest {
+    public static void main(){
+        out.println("第八章1");
+    }
+}
---
+++ JAVA/Chapter8/src/com/mr/game/GameDemo.java
@@ -0,0 +1,11 @@
+package com.mr.game;
+import com.mingrisoft.test.MyTest;
+
+public class GameDemo {
+    public static void main(String[] argv){
+        MyTest test1 = new MyTest();
+        test1.main();
+        System.out.println("第八章2");
+    }
+}
+
